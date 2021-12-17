2a6da2ce6bb27bf1b0095d64c14d4ff80c03a451
Augusto Neftalí Ruiz Cauich <augusto.ruizcauich@gmail.com>
2021-12-17 17:19:29 -0500

    Create application factory function

---
+++ flaskr/__init__.py
@@ -0,0 +1,34 @@
+"""Define the Flask application factory function
+"""
+
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    """Returns a new flask application with an
+    initial configuration.
+    """
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+      pass  
+    
+    @app.route('/welcome')
+    def welcome():
+        return 'Welcome to flask'
+
+    return app

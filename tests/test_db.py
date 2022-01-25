e860355a58e4c1576cd29c0eeff18645a57bf23f
Augusto Neftalí Ruiz Cauich <augusto.ruizcauich@gmail.com>
2022-01-24 22:17:18 -0500

    Add database tests

---
+++ tests/test_db.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+import pytest
+from flaskr.db import get_db
+
+
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT 1')
+
+    assert 'closed' in str(e.value)
+
+
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+
+    def fake_init_db():
+        Recorder.called = True
+
+    # Monkeypatch replace init_db function with fake_init_db.
+    monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
+
+    # When executing the init_db_command function, fake_init_db
+    # will be executed instead of init_db
+    result = runner.invoke(args=['init-db'])
+
+    assert 'Initialized' in result.output
+
+    # As fake_init_db has been called, the called attribute
+    # must be set to True
+    assert Recorder.called
